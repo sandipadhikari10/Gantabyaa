54119f8812893d3d7907336c464db4dde4f5c559
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2024-12-07 11:48:00 +0545

    fix: export as module and name typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-
 import Navbar from './landingpage/Navbar';
 import Login from './login/Login';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 
 import VehicleList from './lists/BikeList';
 import VehicleDetail from './detail/VehicleDetails';
 import CarList from './lists/CarList';
 import FinalBikeList from './lists/FinalBike';
-import AboutUs from './aboutus/Aboutus';
+import AboutUs from './aboutus/AboutUs';
 import Footer from './landingpage/Footer';
 import Home from './landingpage/Home';
 import 'leaflet/dist/leaflet.css';
 
-
-
-
-
-
 function App() {
-  
   return (
-
     <>
-    <Router>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -31,13 +22,13 @@ function App() {
           <Route path="/bikes" element={<FinalBikeList />} />
           <Route path="/cars" element={<CarList />} />
           <Route path="/detail" element={<VehicleList />} />
-        
+
           <Route path="/vehicle/:id" element={<VehicleDetail />} />
           <Route path="/aboutus" element={<AboutUs />} />
         </Routes>
         <Footer />
       </Router>
-   </>
+    </>
   )
 }
 
