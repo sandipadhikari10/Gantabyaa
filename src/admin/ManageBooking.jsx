d1e7d9f2dd5a3009984bad2a4f6f9b90ab7c57ab
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2024-12-21 21:43:57 +0545

    feat: admin manage booking action for unbook

--- src/admin/ManageBooking.jsx
+++ src/admin/ManageBooking.jsx
@@ -3,42 +3,72 @@ import "./ManageBooking.css";
 import Sidebar from "./Sidebar";
 
 const ManageBooking = () => {
-  const [bookings, setBookings] = React.useState([]);
+  const [bookings, setBookings] = React.useState();
+  const [filter, setFilter] = React.useState('');
+  const query = new URLSearchParams({
+    ...(filter && { bookingStatus: filter })
+  }).toString();
 
-  React.useEffect(() => {
-    fetch("/api/admin/booking/list")
+  function fetchBookings() {
+    fetch(`/api/admin/booking/list?${query}`)
       .then((response) => response.json())
       .then((data) => setBookings(data));
-  }, []);
+  }
+  React.useEffect(() => {
+    setBookings(undefined)
+    fetchBookings()
+  }, [filter]);
+
+  function handleUnbook(id) {
+    fetch(`/api/admin/booking/${id}/unbook`, {
+      method: 'POST',
+    })
+      .then(fetchBookings)
+  }
+
   return (
     <div className="main-managebooking">
       <Sidebar />
-
-
       <div className="manage-booking">
         <h1>Manage Booking</h1>
-        <table>
+        <select
+          onChange={(e) => setFilter(e.target.value ? e.target.value : undefined)}
+          defaultValue={filter}
+        >
+          <option value="">All</option>
+          <option value="booked">Booked</option>
+          <option value="pending">Pending</option>
+          <option value="completed">Completed</option>
+        </select>
+        {bookings && <table>
           <thead>
             <tr>
-              <th>Name</th>
+              <th>Booked By</th>
               <th>Vehicle</th>
               <th>From</th>
               <th>To</th>
               <th>Status</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <tr key={booking.id}>
-                <td>{booking.name}</td>
-                <td>{booking.vehicle}</td>
+              <tr key={booking._id}>
+                <td>{booking.user.name}</td>
+                <td>{booking.vehicle.name}</td>
                 <td>{booking.from}</td>
                 <td>{booking.to}</td>
                 <td>{booking.status}</td>
+                <td>
+                  <div>
+                    <button className="btn btn-danger" onClick={() => handleUnbook(booking._id)}>Unbook</button>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
-        </table>
+        </table>}
+        {!bookings && <h2>Fetching booking data...</h2>}
       </div>
     </div>
   );
