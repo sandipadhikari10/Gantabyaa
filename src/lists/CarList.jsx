78808341596c3acae511b13c1e84d82383042b76
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2025-01-11 15:37:45 +0545

    feat: search vehicles frontend

--- src/lists/CarList.jsx
+++ src/lists/CarList.jsx
@@ -1,35 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CarList.css';
 import { Link } from 'react-router-dom';
 
 
 const CarList = () => {
   const [carData, setCarData] = React.useState([]);
+  const [searchQuery, setSearchQuery] = useState("")
   useEffect(() => {
-    fetch("/api/vehicles?type=car")
+    fetch(`/api/vehicles?type=car&searchQuery=${searchQuery}`)
       .then((response) => response.json())
       .then((data) => setCarData(data));
-  }, []);
+  }, [searchQuery]);
 
 
   return (
     <>
-      <div className="car-list">
-        {carData.map((car) => (
-          <div key={car._id} className="car-card">
-            <Link to={`/vehicle/${car._id}`} className='info'>
-              <img src={car.image} alt={car.name} className="car-image" />
-              <h3 className='carName'>{car.name}</h3>
-              <div className="car-details">
-                <div className='cardetails-show'>
-                  <div><p>Free Cancelation</p>
-                    <p>Instant Booking</p></div>
+      <div style={{ padding: "100px", display: 'flex', flexDirection: 'column' }}>
+        <div style={{ width: '100%', margin: '10px 0px' }}>
+          Search:
+          <input type="string" value={searchQuery} style={{ width: '100%' }} onChange={e => setSearchQuery(e.currentTarget.value)} />
+        </div>
+        <div className="bike-list" style={{ padding: "100px" }}>
+          {!!carData.length ? carData.map((car) => (
+            <div key={car._id} className="car-card">
+              <Link to={`/vehicle/${car._id}`} className='info'>
+                <img src={car.image} alt={car.name} className="car-image" />
+                <h3 className='carName'>{car.name}</h3>
+                <div className="car-details">
+                  <div className='cardetails-show'>
+                    <div><p>Free Cancelation</p>
+                      <p>Instant Booking</p></div>
+                  </div>
+                  <div className="car-price">Rs. {car.price} per day</div>
                 </div>
-                <div className="car-price">Rs. {car.price} per day</div>
-              </div>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          )) : <div>No data to show</div>}
+        </div>
       </div>
     </>
   );
--- src/lists/FinalBike.jsx
+++ src/lists/FinalBike.jsx
@@ -4,35 +4,42 @@ import { Link } from "react-router-dom";
 
 const FinalBikeList = () => {
   const [bikeData, setBikeData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("")
 
   useEffect(() => {
-    fetch("/api/vehicles?type=bike")
+    fetch(`/api/vehicles?type=bike&searchQuery=${searchQuery}`)
       .then((response) => response.json())
       .then((data) => setBikeData(data));
-  }, []);
+  }, [searchQuery]);
 
   return (
     <>
-      <div className="bike-list" style={{ padding: "100px" }}>
-        {!!bikeData.length ? bikeData.map((bike) => (
-          <div key={bike.id} className="bike-card">
-            <Link to={`/vehicle/${bike._id}`} className='info'>
-              <img src={bike.image} alt={bike.name} className="bike-image" />
-              <h3 className='bikeName'>{bike.name}</h3>
-              <div className="bike-details">
-                <div className='bikedetails-show'>
-                  <div><p>Free Cancelation</p>
-                    <p>Instant Booking</p></div>
-                  <div><p>{bike.description}</p>
-                    <p>{bike.type}</p></div>
+      <div style={{ padding: "100px", display: 'flex', flexDirection: 'column' }}>
+        <div style={{ width: '100%', margin: '10px 0px' }}>
+          Search:
+          <input type="string" value={searchQuery} style={{ width: '100%' }} onChange={e => setSearchQuery(e.currentTarget.value)} />
+        </div>
+        <div className="bike-list" style={{ padding: "100px" }}>
+          {!!bikeData.length ? bikeData.map((bike) => (
+            <div key={bike.id} className="bike-card">
+              <Link to={`/vehicle/${bike._id}`} className='info'>
+                <img src={bike.image} alt={bike.name} className="bike-image" />
+                <h3 className='bikeName'>{bike.name}</h3>
+                <div className="bike-details">
+                  <div className='bikedetails-show'>
+                    <div><p>Free Cancelation</p>
+                      <p>Instant Booking</p></div>
+                    <div><p>{bike.description}</p>
+                      <p>{bike.type}</p></div>
+                  </div>
+
+                  <div className="bike-price">Rs. {bike.pricePerDay} per day</div>
                 </div>
+              </Link>
+            </div>
 
-                <div className="bike-price">Rs. {bike.pricePerDay} per day</div>
-              </div>
-            </Link>
-          </div>
-
-        )) : <h2>No data to show</h2>}
+          )) : <h2>No data to show</h2>}
+        </div>
       </div>
     </>
   );
